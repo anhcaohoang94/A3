b538a40ea75c511ed1f9230c572fccdb11279706
anhcaohoang94 <50747566+anhcaohoang94@users.noreply.github.com>
2020-04-16 05:24:33 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,8 @@
+import {songs} from '../../app/songInfo';
+
+export const SONGS: songs[] = [
+    {name: 'Smack That', artist: 'Akon', genre: 'Pop', year: '2006', picture: '../../assets/images/Eminem.jfif'},
+    {name: 'Motherlover', artist: 'The Lonely Island', genre: 'Dance/Electronic', year: '2011', picture: '../../assets/images/Motherlover.png'},
+    {name: 'Sexy Back', artist: 'Justin Timberblake', genre: 'Pop', year: '2006', picture: '../../assets/images/Justin.jpg'},
+    {name: 'Kiss Kiss', artist: 'Chris Brown', genre: 'R&B', year: '2007', picture: '../../assets/images/Chris.jpg'}
+];
